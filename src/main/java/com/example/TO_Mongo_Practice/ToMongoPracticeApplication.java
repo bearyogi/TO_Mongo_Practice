9c5eb8de1a1ad9e4051e71c1bf3c8d6e7e9019cd
Michał Mamla <mmichal1999@gmail.com>
2020-12-17 17:28:42 +0100

    Initial commit

---
+++ src/main/java/com/example/TO_Mongo_Practice/ToMongoPracticeApplication.java
@@ -0,0 +1,13 @@
+package com.example.TO_Mongo_Practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToMongoPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToMongoPracticeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/TO_Mongo_Practice/ToMongoPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.TO_Mongo_Practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToMongoPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
